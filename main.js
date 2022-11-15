706a26a9b88999143a9c2de31f9af98e7e69c8bd
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-15 11:38:19 -0500

    building game with JS

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("loaded")
+
+const mainGame = document.querySelector(".main-game");
+const totalScore = document.querySelector(".total")
+
+
+
+function addCategory(category) {
+    const column = document.createElement("div");
+    column.classList.add("category-column");
+
+    const subjectTitle = document.createElement("div");
+    subjectTitle.classList.add("category-title")
+    subjectTitle.innerHTML = category.subject
+}
