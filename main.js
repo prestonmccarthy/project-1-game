ca0c75d0d1833b77a438f9d81889f0cad6b95e7a
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-15 16:47:28 -0500

    Styling with grid to build structure

--- main.js
+++ main.js
@@ -3,13 +3,6 @@ console.log("loaded")
 const mainGame = document.querySelector(".main-game");
 const totalScore = document.querySelector(".total")
 
+const triviaCategories = []
 
 
-function addCategory(category) {
-    const column = document.createElement("div");
-    column.classList.add("category-column");
-
-    const subjectTitle = document.createElement("div");
-    subjectTitle.classList.add("category-title")
-    subjectTitle.innerHTML = category.subject
-}
