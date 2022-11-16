eb2a500bd687a8ca1d2d3a73d95ce0ba2b8ddc8b
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-16 14:28:39 -0500

    removing css styling and starting game over, building with JS

--- main.js
+++ main.js
@@ -1,222 +1,250 @@
 console.log("loaded")
 
-const mainGame = document.querySelector(".main-game");
-const totalScore = document.querySelector(".total")
+const game = document.querySelector(".game");
+const scoreTotal = document.querySelector(".score");
 
 const triviaCategories = [
-    {
-      //house in a seperate file and reference "constants.js"
-      genre: "NUMBERS GAME",
-      questions: [
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 200,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 400,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 600,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 800,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 1000,
-        },
-      ],
-    },
-    {
-      genre: "TYPES",
-      questions: [
-        {
-            question: `Preston Test`,
-            choices: ["Option1", "Test2", "Option3", "Test4"],
-            correct: "Test2",
-            rank: 200,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 400,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 600,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 800,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 1000,
-        },
-      ],
-    },
-    {
-      genre: "GAME TIME",
-      questions: [
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 200,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 400,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 600,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 800,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 1000,
-        },
-      ],
-    },
-    {
-      genre: "EEVEE-LUTIONS",
-      questions: [
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 200,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 400,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 600,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 800,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 1000,
-        },
-      ],
-    },
-    {
-      genre: "CHAMPIONS",
-      questions: [
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 200,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 400,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 600,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 800,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 1000,
-        },
-      ],
-    },
-    {
-      genre: "ANIME",
-      questions: [
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 200,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 400,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 600,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 800,
-        },
-        {
-            question: `Question Text`,
-            choices: ["Option1", "Option2", "Option3", "Option4"],
-            correct: "Correct Answer",
-            rank: 1000,
-        },
-      ],
-    },
-  ];
-  
+  {
+    //house in a seperate file and reference "constants.js"
+    category: "NUMBERS GAME",
+    questions: [
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 200,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 400,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 600,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 800,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 1000,
+      },
+    ],
+  },
+  {
+    category: "TYPES",
+    questions: [
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 200,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 400,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 600,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 800,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 1000,
+      },
+    ],
+  },
+  {
+    category: "GAME TIME",
+    questions: [
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 200,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 400,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 600,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 800,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 1000,
+      },
+    ],
+  },
+  {
+    category: "EEVEE-LUTIONS",
+    questions: [
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 200,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 400,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 600,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 800,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 1000,
+      },
+    ],
+  },
+  {
+    category: "CHAMPIONS",
+    questions: [
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 200,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 400,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 600,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 800,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 1000,
+      },
+    ],
+  },
+  {
+    category: "ANIME",
+    questions: [
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 200,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 400,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 600,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 800,
+      },
+      {
+        question: `Question`,
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "Answer 1",
+        points: 1000,
+      },
+    ],
+  },
+];
 
+let score = 000;
+//------------------------------- Creating Columns ---------------------------
+function addTitle(genre) {
+  const column = document.createElement("div"); // creates column div and adds class
+  column.classList.add("title-column");
 
-let total = 0;
+  const titleTitle = document.createElement("div");
+  titleTitle.classList.add("title-title");
+  titleTitle.innerText = genre.category;
+
+  column.appendChild(titleTitle);
+  game.append(column);
+
+  genre.questions.forEach((question) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    column.appendChild(card);
+    card.innerText = question.points;
+
+    card.setAttribute("question-question", question.question);
+    question.choices.forEach((choice, index) =>
+    card.setAttribute(`question-option-${index + 1}`, choice)); //sets question-option to each question
+    card.setAttribute("data-correct", question.correct);
+    card.setAttribute("data-value", card.getInnerHTML());
+
+    card.addEventListener("click", showQuestion);
+  });
+}
+
+triviaCategories.forEach((genre) => addTitle(genre)); // adds the function above
+
