3789d990e38b841486dcc1c1279a5ed35991bddf
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-17 16:20:49 -0500

    styling for basic gameplay done. Working on Question Styling

--- main.js
+++ main.js
@@ -13,33 +13,33 @@ const triviaCategories = [
     category: "NUMBERS GAME",
     questions: [
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This Pokémon, the face of the franchise, is #25 in the Pokédex.`,
+        choices: ["Jigglypuff", "Sandshrew", "Pikachu", "Raticate"],
+        correct: "Pikachu",
         points: 200,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This Three-headed Flying-type Pokémon ranks at #85.`,
+        choices: ["Dugtrio", "Dodrio", "Fearow", "Articuno"],
+        correct: "Dodrio",
         points: 400,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This Ghost-type Pokémon is #93 and can only evolve when traded to another player.`,
+        choices: ["Machoke", "Haunter", "Shedinja", "Sableye"],
+        correct: "Haunter",
         points: 600,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Pokémon #143 is known as this sleeping giant that only awakens with the help of the Poké Flute.`,
+        choices: ["Kangaskhan", "Snorlax", "Magmar", "Jynx"],
+        correct: "Snorlax",
         points: 800,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Pokémon #151 is this Pokémon which cannot be naturally captured in the first games (Red/Blue).`,
+        choices: ["Mewtwo", "Dratini", "Moltres", "Mew"],
+        correct: "Mew",
         points: 1000,
       },
     ],
@@ -48,33 +48,33 @@ const triviaCategories = [
     category: "TYPES",
     questions: [
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `The most recently released typing in Pokémon is this.`,
+        choices: ["Poison", "Dark", "Fairy", "Steel"],
+        correct: "Fairy",
         points: 200,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Bug-type Pokémon are weak to Flying-type and this type of attacks.`,
+        choices: ["Dark", "Psychic", "Fighting", "Fire"],
+        correct: "Fire",
         points: 400,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Venasaur, Venonat, and Arbok all share this type.`,
+        choices: ["Flying", "Poison", "Bug", "Grass"],
+        correct: "Poison",
         points: 600,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This starter Pokémon from Red/Blue is the only starter that does not have two types by its final evolution.`,
+        choices: ["Squirtle", "Pikachu", "Bulbasaur", "Charmander"],
+        correct: "Squirtle",
         points: 800,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Fairy-type Pokémon are weak to Poison and this type.`,
+        choices: ["Steel", "Fighting", "Psychic", "Dark"],
+        correct: "Steel",
         points: 1000,
       },
     ],
@@ -83,33 +83,33 @@ const triviaCategories = [
     category: "GAME TIME",
     questions: [
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `What is the first game where the player can catch Charmander, Bulbasaur, and Squirtle without trading?`,
+        choices: ["Leaf Green", "Blue", "Red", "Yellow"],
+        correct: "Yellow",
         points: 200,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `In which games do the running shoes first make an appearance?`,
+        choices: ["Gold/Silver", "Red/Blue", "Ruby/Sapphire", "Black/White"],
+        correct: "Gold/Silver",
         points: 400,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Lieutenant Surge is the gym leader of this city.`,
+        choices: ["Saffron City", "Vermillion City", "Celadon City", "Viridian City"],
+        correct: "Vermillion City",
         points: 600,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Mewtwo is first encountered in this cave after defeating the Pokémon League in Red/Blue.`,
+        choices: ["Victory Road", "Mt. Moon", "Cerulean Cave", "Diglett's Cave"],
+        correct: "Cerulean Cave",
         points: 800,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This game, from the first series was released in Japan, but not the USA.`,
+        choices: ["Red", "Blue", "Green", "Yellow"],
+        correct: "Green",
         points: 1000,
       },
     ],
@@ -118,33 +118,33 @@ const triviaCategories = [
     category: "EEVEE-LUTIONS",
     questions: [
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This Pokémon evolves from Eevee after being given a water stone.`,
+        choices: ["Umbreon", "Jolteon", "Vaporeon", "Glaceon"],
+        correct: "Vaporeon",
         points: 200,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `How many total Eevee-lutions are there currently?`,
+        choices: ["4", "6", "10", "8"],
+        correct: "8",
         points: 400,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Flareon, Vaporeon, and Jolteon were all released in these games.`,
+        choices: ["Red/Blue", "Gold/Silver", "Ruby/Sapphire", "Diamond/Pearl"],
+        correct: "Red/Blue",
         points: 600,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Eevee evolves into this Pokémon by raising friendship with its trainer in the day time.`,
+        choices: ["Umbreon", "Leafeon", "Sylveon", "Espeon"],
+        correct: "Espeon",
         points: 800,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This Pokémon is the most recently released evolution of Eevee.`,
+        choices: ["Leafeon", "Glaceon", "Sylveon", "Espeon"],
+        correct: "Sylveon",
         points: 1000,
       },
     ],
@@ -153,33 +153,33 @@ const triviaCategories = [
     category: "CHAMPIONS",
     questions: [
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This dragon-type trainer is the Johto Pokémon League Champion in Pokémon Gold & Silver.`,
+        choices: ["Drayden", "Clair", "Iris", "Lance"],
+        correct: "Lance",
         points: 200,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `In which game(s) does the Pokémon League Champion specialize in Steel-type Pokémon?`,
+        choices: ["Red/Blue", "Ruby/Sapphire", "Diamon/Pearl", "Black/White"],
+        correct: "Ruby/Sapphire",
         points: 400,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Who is the champion of the Kanto region (Pokémon Red/Blue)?`,
+        choices: ["Player's Rival", "Giovanni", "Professor Oak", "Player's Mom"],
+        correct: "Player's Rival",
         points: 600,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `This Champion wanders the Galar region with his trusty Charizard.`,
+        choices: ["Lance", "Leon", "Blue", "Alder"],
+        correct: "Leon",
         points: 800,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `In Pokémon Ruby & Sapphire, you're given access to Beldum after defeating this Pokémon League Champion.`,
+        choices: ["Alder", "Iris", "Steven", "Cynthia"],
+        correct: "Steven",
         points: 1000,
       },
     ],
@@ -188,33 +188,33 @@ const triviaCategories = [
     category: "ANIME",
     questions: [
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Ash Ketchum begins his journey by leaving this town with his trusty companion, Pikachu.`,
+        choices: ["Lavender Town", "Pallet Town", "Mahogany Town", "Littleroot Town"],
+        correct: "Pallet Town",
         points: 200,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Ash earns his first badge from this Rock-type gym leader.`,
+        choices: ["Brawly", "Jasmine", "Brock", "Roark"],
+        correct: "Brock",
         points: 400,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `What is the first Pokémon Ash ever caught?`,
+        choices: ["Pidgey", "Caterpie", "Pikachu", "Mankey"],
+        correct: "Caterpie",
         points: 600,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `Team Rocket's Jessie and James trained these two Pokémon in the first season of the series.`,
+        choices: ["Ekans & Koffing", "Seviper & Woobat", "Wobbufet & Carnivine", "Lickitung & Bewear"],
+        correct: "Ekans & Koffing",
         points: 800,
       },
       {
-        question: `Question`,
-        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        correct: "Answer 1",
+        question: `These two Pokémon are known for being featured in the only two episodes that were taken off the air.`,
+        choices: ["Tentacruel & Venonat", "Spearow & Unown", "Porygon & Jynx", "Raichu & Wigglytuff"],
+        correct: "Porygon & Jynx",
         points: 1000,
       },
     ],
@@ -248,33 +248,33 @@ function showQuestion(event) {
     const optionThree = document.createElement("button");
     const optionFour = document.createElement("button");
   
-    optionOne.classList.add("option-one"); 
+    optionOne.classList.add("option-one");
     optionTwo.classList.add("option-two");
     optionThree.classList.add("option-three");
     optionFour.classList.add("option-four");
   
-    optionOne.innerText = this.getAttribute("question-option-1"); 
+    optionOne.innerText = this.getAttribute("question-option-1");
     optionTwo.innerText = this.getAttribute("question-option-2");
     optionThree.innerText = this.getAttribute("question-option-3");
     optionFour.innerText = this.getAttribute("question-option-4");
   
-    optionOne.addEventListener("click", getAnswer); 
+    optionOne.addEventListener("click", getAnswer);
     optionTwo.addEventListener("click", getAnswer);
     optionThree.addEventListener("click", getAnswer);
     optionFour.addEventListener("click", getAnswer);
   
-    optionOne.addEventListener("click", toggleModal); 
+    optionOne.addEventListener("click", toggleModal);
     optionTwo.addEventListener("click", toggleModal);
     optionThree.addEventListener("click", toggleModal);
     optionFour.addEventListener("click", toggleModal);
   
-    modalInside.innerText = ""; 
+    modalInside.innerText = "";
     modalInside.dataset.correct = this.getAttribute("data-correct");
     modalInside.dataset.value = this.getAttribute("data-value");
-    modalInside.append(questionDisplay, optionOne, optionTwo, optionThree, optionFour); 
-    modalContainer.classList.remove("hidden"); 
+    modalInside.append(questionDisplay, optionOne, optionTwo, optionThree, optionFour);
+    modalContainer.classList.remove("hidden");
   
-    const myCards = Array.from(document.querySelectorAll(".card")); 
+    const myCards = Array.from(document.querySelectorAll(".card"));
     myCards.forEach((card) => card.removeEventListener("click", showQuestion));
     event.target.children.length = 0;
 }
